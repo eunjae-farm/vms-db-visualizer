f0362fb030e171862ba7654c1c54906d2063daf3
Piorosen <chacha@udon.party>
2023-10-06 11:29:58 +0900

    유니티 관련 모델링 작업 완성함

--- Unity/Assets/Scripts/RandomVibrator.cs
+++ Unity/Assets/Scripts/RandomVibrator.cs
@@ -17,40 +17,72 @@ public class RandomVibrator : MonoBehaviour
 
     public float RefreshTick = 2;
     public float Amp = 5;
-    public float WingRotatePerSeconds = 360;
+    public float WingRotatePerSeconds = 1;
 
     private float cur_refresh_tick = 0;
-    private float cur_wing_rotate = 0;
 
-    // Start is called before the first frame update
+    // Start is called before the first frame updatew
     void Start()
     {
-        
+        cur_rot = new List<float>
+        {
+            0,0,0,0,0,0,0,0,
+        };
     }
-    //삼각형을 형성하는 세 점의 배열을 만듭니다.
-    Vector3[] points = new Vector3[]
-           {
-            new Vector3(-1, -1, 0),
-            new Vector3(1, -1, 0),
-            new Vector3(0, 1, 0)
-           };
+    (Vector3, GameObject) ComputeCenterPosition(GameObject obj)
+    {
+        var center = Wing.GetComponentsInChildren<Renderer>();
+        Vector3 c = new Vector3();
+        foreach (Renderer r in center)
+        {
+            c += r.bounds.center;
+        }
+        c /= center.Length;
+        return (c,  obj);
+    }
+
+    bool v = false;
+    List<float> cur_rot = new List<float>();
 
 
     // Update is called once per frame
     void Update()
     {
         cur_refresh_tick += Time.deltaTime;
-        cur_wing_rotate += Time.deltaTime;
-
-        if (cur_refresh_tick > RefreshTick)
-        {
-
-        }
-        if (cur_wing_rotate > WingRotatePerSeconds)
-        {
-            cur_wing_rotate -= WingRotatePerSeconds;
-        }
-        this.transform.localEulerAngles = new Vector3(0, 0, cur_wing_rotate * 360);
         
+        var w = ComputeCenterPosition(Wing);
+        this.Wing.transform.RotateAround(w.Item1, Vector3.fwd, Time.deltaTime * WingRotatePerSeconds * 360);
+
+        if (cur_refresh_tick > (1 / RefreshTick))
+        {
+            cur_refresh_tick = 0;
+            v = !v;
+
+            var fb = ComputeCenterPosition(FisrtBearing);
+            var sb = ComputeCenterPosition(SecondBearing);
+            var g = ComputeCenterPosition(Generator);
+            var bo = ComputeCenterPosition(BaseObject);
+            var rp = ComputeCenterPosition(RedPivot);
+            //var h = ComputeCenterPosition(Hole);
+            var b = ComputeCenterPosition(Body);
+            var e = ComputeCenterPosition(Electro);
+
+            var shake = new List<(Vector3, GameObject)>
+            {
+                fb,sb,g,bo,rp,b,e,
+            };
+
+            for (int i = 0; i < shake.Count; i++) { 
+                var amp = (Random.value * Amp) - (Amp / 2);
+                var cur = cur_rot[i];
+                var obj = shake[i].Item2;
+                var pos = shake[i].Item1;
+
+                obj.transform.RotateAround(pos, Vector3.fwd, amp - cur);
+                cur_rot[i] = amp;
+            }
+
+        }
+
     }
 }
