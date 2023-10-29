d260a289bae24be0fbbb5a491136a12446e0be6b
Joo Hyoung Cha <chacha@udon.party>
2023-10-29 22:42:37 +0900

    Feature/manager (#5) (#6)
    * 콜백기능구현함
    
    * 페이지네이셔닝 구현

---
+++ Unity/Assets/Scripts/SceneManager/SceneManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Diagnostics;
+using UnityEngine;
+
+public abstract class SceneManager : MonoBehaviour
+{
+    public abstract void Enable();
+    public abstract void Disable();
+
+}
