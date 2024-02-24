e504d06bdd8ebd428a0d7844222b4a44327dd1bb
Piorosen <chacha@udon.party>
2024-02-25 02:51:25 +0900

    Update TurbineConnectionDataManager.cs

--- Unity/Assets/Scripts/TurbineManage/Data/TurbineConnectionDataManager.cs
+++ Unity/Assets/Scripts/TurbineManage/Data/TurbineConnectionDataManager.cs
@@ -22,6 +22,34 @@ public class TurbineConnectionDataManager : SingleTon<TurbineConnectionDataManag
         var path = Path.Combine(dataPath, fileName);
         if (!File.Exists(path))
         {
+            Data.Add(new TurbineConnectionData
+            {
+                DBIP = "59.28.91.19",
+                DBName = "Hangwon4",
+                FastRotateSpeed = 1000,
+                ID = "sa",
+                PW ="skf1234!",
+                MagnitudeOfCorrectForMotion = new List<float>
+                {
+                    0.5f,0.5f,0.5f,0.5f,0.5f
+                },
+                MagnitudeOfErrorForMotion = new List<float>
+                {
+                    2f,2f,2f,2f,2f
+                },
+                Name = "Ç×¿ø Ç³·Â",
+                NodeId = 4,
+                NodeName = "U88_H4",
+                SlowRotateSpeed = 0.1f,
+                ObserveBearing = new List<string>
+                {
+                    "MB_H_VEL",         "MB_V_VEL",         "MB_A_VEL",
+                    "GB_H_Low_LVEL",    "GB_V_Low_LVEL",    "GB_A_Low_LVEL",
+                    "GB_HSS_H_VEL",     "GB_HSS_V_VEL",     "GB_A_Low_LVEL",
+                    "GE_RS_H_VEL",      "GE_RS_V_VEL",      "GE_RS_A_VEL",
+                    "GE_GS_H_VEL",      "GE_GS_V_VEL",      "GE_RS_A_VEL"
+                }
+            });
             Save();
         }
 
