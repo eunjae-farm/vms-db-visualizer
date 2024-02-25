14ab8260cb49c020ac2689f48006bccb0d1c44f3
Joo Hyoung Cha <aoikazto@naver.com>
2024-02-26 02:51:43 +0900

    Update RandomGeneratorMotion.cs

--- Unity/Assets/Scripts/Viewer/Turbin/RandomGeneratorMotion.cs
+++ Unity/Assets/Scripts/Viewer/Turbin/RandomGeneratorMotion.cs
@@ -387,19 +387,18 @@ public class RandomGeneratorMotion : MonoBehaviour
 
         foreach (var item in Bearing[0].list)
         {
-            var p = Quaternion.LookRotation(-pc1);
+            var p = Quaternion.LookRotation(-pc1).eulerAngles;
             p.z = 0;
             item.transform.localPosition = vec[0] + va;
-            item.transform.localRotation = p;
+            item.transform.localRotation = Quaternion.Euler(p);
             // prevLocal[0] = vec[0];
         }
         
         foreach (var item in Bearing[1].list)
         {
-            var p = Quaternion.LookRotation(-pd1);
+            var p = Quaternion.LookRotation(-pd1).eulerAngles;
             p.z = 0;
-            // p.z = 0;
-            item.transform.localRotation = p;
+            item.transform.localRotation = Quaternion.Euler(p);
             // RotateObject(item.transform, Quaternion.LookRotation((c2 + vec[2]) - (c1 + vec[1])).eulerAngles);
             item.transform.localPosition =  (vec[1] + vec[2]) / 2 + va;
             // prevLocal[1] = vec[1];
@@ -407,9 +406,9 @@ public class RandomGeneratorMotion : MonoBehaviour
         
         foreach (var item in Bearing[2].list)
         {
-            var p = Quaternion.LookRotation(-pd2);
+            var p = Quaternion.LookRotation(-pd2).eulerAngles;
             p.z = 0;
-            item.transform.localRotation = p;
+            item.transform.localRotation = Quaternion.Euler(p);
             // RotateObject(item.transform, Quaternion.LookRotation((c3 + vec[4]) - (c2 + vec[3])).eulerAngles);
             item.transform.localPosition = (vec[3] + vec[4]) / 2 + va;
             // prevLocal[2] = vec[2];
